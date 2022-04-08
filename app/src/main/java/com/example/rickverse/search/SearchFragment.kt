3347ce60fa3c8282e5aa80b5ac1f50414bb5243d
Joao Pedro Haeser Iserhard <jiserhard@gmail.com>
2022-04-08 20:07:47 -0300

    Adicionado validações no formulario de busca

--- app/src/main/java/com/example/rickverse/search/SearchFragment.kt
+++ app/src/main/java/com/example/rickverse/search/SearchFragment.kt
@@ -5,10 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.widget.doAfterTextChanged
 import com.example.rickverse.R
+import com.example.rickverse.extension.enabledChildren
+import com.example.rickverse.extension.hideError
+import com.example.rickverse.extension.showError
+import com.google.android.material.textfield.TextInputLayout
+import kotlinx.android.synthetic.main.fragment_search.*
+import java.io.ObjectInputValidation
 
+private const val MINIMUM_CHARACTERS_SEARCH = 3
 
 class SearchFragment : Fragment() {
+
+    private var isNameValid = false
+    private var isSpeciesValid = false
+    private var isStatusValid = false
 
 
     override fun onCreateView(
@@ -16,5 +30,88 @@ class SearchFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(btnSearch) {
+            isEnabled = false
+            setOnClickListener {
+                Toast.makeText(context, "Vai reealizar a busca", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        with(rgStatus) {
+            enabledChildren(false)
+            setOnCheckedChangeListener { _, _ ->
+                isStatusValid = checkedRadioButtonId != 1
+                updateButtonState()
+                //verifica se o botão pode ser habilidade
+
+
+            }
+        }
+
+        swStatus.setOnCheckedChangeListener { _, isChecked ->
+            rgStatus.enabledChildren(isChecked)
+            if (isChecked.not()) {
+                rgStatus.clearCheck()
+            }
+        }
+
+        verifyEditTextValue(editText = tieName, textInputLayout = tilName) { value ->
+            isNameValid = value.length >= MINIMUM_CHARACTERS_SEARCH
+            isNameValid
+        }
+
+        verifyEditTextValue(editText = tieSpecie, textInputLayout = tilSpecie) { value ->
+            isSpeciesValid = value.length >= MINIMUM_CHARACTERS_SEARCH
+            isSpeciesValid
+        }
+
+    }
+
+    private fun verifyEditTextValue(
+        editText: EditText,
+        textInputLayout: TextInputLayout,
+        validation: ((value: String) -> Boolean)
+    ) {
+        editText.doAfterTextChanged {
+            verifyValueAndSetError(
+                value = it.toString(),
+                textInputLayout = textInputLayout,
+                validation = validation
+            )
+        }
+        editText.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                textInputLayout.hideError()
+            } else {
+                verifyValueAndSetError(
+                    value = editText.text.toString(),
+                    textInputLayout = textInputLayout,
+                    validation = validation,
+                    showError = true
+                )
+            }
+        }
+    }
+
+    private fun verifyValueAndSetError(
+        value: String,
+        showError: Boolean = false,
+        textInputLayout: TextInputLayout,
+        validation: ((value: String) -> Boolean)
+    ) {
+        if (value.isNotEmpty() && validation(value).not() && showError) {
+            textInputLayout.showError(getString(R.string.minimum_character_search_error))
+        }
+        updateButtonState()
+
+    }
+
+    private fun updateButtonState() {
+        btnSearch.isEnabled = isNameValid || isSpeciesValid || isStatusValid
     }
 }
